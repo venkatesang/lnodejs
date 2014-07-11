0104145f9a2a409d7f6fca43770596d38dad2d69
venkatesang <venkatesancg@gmail.com>
2014-07-11 16:06:55 +0530

    created server module like  node js module

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var http = require('http');
 
+function start(){
+    function onRequest(request,response){
+        console.log('request received');
+        response.writeHead(200, {'content-type':'text/plain'});
+        response.write("Hello Aditi");
+        response.end();
+    }
 
-function onRequest(request,response){
-    console.log('request received');
-    response.writeHead(200, {'content-type':'text/plain'});
-    response.write("Hello Aditi");
-    response.end();
+    http.createServer(onRequest).listen(8888);
+    console.log("Server Started");
+
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("Server Started");
-    
-    
+exports.start = start;
+
+
+
